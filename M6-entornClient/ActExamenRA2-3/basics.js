23a0a67a3f6f555aa79cb4895dbfd61038fdcf7a
Marta-daw <marta.llab.daw@gmail.com>
2025-12-14 21:39:15 +0100

    temp

--- M6-entornClient/ActExamenRA2-3/basics.js
+++ M6-entornClient/ActExamenRA2-3/basics.js
@@ -3,11 +3,85 @@
     • Almenys una lletra majúscula 
     • Almenys un número 
     • Almenys un caràcter especial (@, #, $, %, &) */
+function validarContrasenya(contrasenya){
+    const largoContra=contrasenya.length;
+    const simbol=/[@, #, $, %, &]/g; //expressió regular per buscar símbols on la g 
+    
+    let tieneMayuscula=false;
+    let tieneNumero=false;
+    for (let i=0; i<contrasenya.length; i++){
+        const char=contrasenya.charAt(i);
+        if (char >= 'A' && char <= 'Z'){
+            tieneMayuscula=true;
+        } else if (char >= '0' && char <= '9'){
+            tieneNumero=true;
+        }
+    }
+
+    let resposta;
+
+    if (largoContra>= 8 && contrasenya.match(simbol) && tieneMayuscula && tieneNumero){
+        resposta = "Contrasenya CORRECTE";
+    } else {
+        resposta="Contrasenya INCORRECTE";
+    }
+
+    return resposta;
+}
+
+const contrasenya = "Marta$25";
+const valid = validarContrasenya(contrasenya);
+console.log(valid);
+console.log("-----------------------------------------------")
 
 /* 2. Calculadora d'edat: Crea un script que, donada una data de naixement, calculi:
     • L'edat exacta en anys 
     • Els dies que falten pel proper aniversari 
     • El dia de la setmana en què vas néixer  */
+
+    function calculadoraEdat(dataNaixement){
+        const arrayFecha = dataNaixement.split("/");
+
+        const dayActual = new Date;
+
+        const diaActual = dayActual.getDate();
+        const mesActual = dayActual.getMonth()+1;
+        const anoActual = dayActual.getFullYear();
+
+        let missatgeA="";
+
+        // Per saber l'edat actual de la persona
+        if (mesActual < arrayFecha[1] || (mesActual == arrayFecha[1] && diaActual < arrayFecha[0])){
+            const edat = anoActual - arrayFecha[2] - 1;
+            missatgeA = "Tens "+edat+" anys.";
+        } else {
+            const edat = anoActual - arrayFecha[2];
+            missatgeA = "Tens "+edat+" anys.";
+        }
+
+        // Per saber els dies que falten pel proper aniversari
+        const properAniversari = new Date(anoActual, arrayFecha[1]-1, arrayFecha[0]);
+
+        if (mesActual > arrayFecha[1] || (mesActual == arrayFecha[1] && diaActual > arrayFecha[0])){
+            properAniversari.setFullYear(anoActual + 1);
+        }
+        const tempsRestant = properAniversari - dayActual;
+        const diesRestants = Math.ceil(tempsRestant / (1000 * 60 * 60 * 24));
+        missatgeA += " Falten "+diesRestants+" dies pel teu proper aniversari.";
+
+        // Per saber el dia de la setmana en què vas néixer
+        const diaSetmana = new Date(arrayFecha[2], arrayFecha[1]-1, arrayFecha[0]);
+        const diesSetmana = ["Diumenge", "Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte"];
+        const diaNaixement = diesSetmana[diaSetmana.getDay()];
+        missatgeA += " Vas néixer un "+diaNaixement+".";
+
+        return missatgeA;
+    }
+
+const dataNaixement = "10/06/1995";
+const calcularEdat = calculadoraEdat(dataNaixement);
+console.log(calcularEdat);
+console.log("-----------------------------------------------")
 
 /* 3. Manipulació de text: Donada una frase:
     • Compta quantes vocals té 
@@ -15,11 +89,59 @@
     • Inverteix l'ordre de les paraules 
     • Crea un acrònim amb les primeres lletres de cada paraula  */
 
+    function manipulacioText (frase){
+        const fraseMin = frase.toLowerCase();
+        const letrasFrase= fraseMin.split("");
+
+        let count=0;
+
+        //Per contar les vocals
+        message="";
+        for (let i=0; i<letrasFrase.length; i++){
+            if (letrasFrase[i] == "a" || letrasFrase[i] == "e" || letrasFrase[i] == "i" || letrasFrase[i] == "o" || letrasFrase[i] == "u"){
+                count++;
+            }
+        }
+        message = "La frase té "+count+" vocals.\n";
+
+        //Per substituir les vocals per l'asterisc
+        const subsFrase= fraseMin.replace(/[aeiou]/g, "*");
+
+        message += "Substituim les vocals per '*': "+subsFrase;
+
+        // invertir l'ordre
+        const paraulesFrase = frase.split(" ");
+
+        let fraseInvers= [];
+
+        for (let i=1; i<= paraulesFrase.length; i++){
+            fraseInvers.push(paraulesFrase.slice(-i)[0]);
+        }
+
+        fraseInvers = fraseInvers.join(" "); //Passar l'array a un string
+        message += "\nFrase invertida: "+fraseInvers;
+        
+        //Crear acrònim
+        let acronim="";
+
+        for (let i=0; i<paraulesFrase.length; i++){
+            acronim += paraulesFrase[i].charAt(0).toUpperCase(); //.charAt(0) per agafar la primera lletra de cada paraula
+        }
+
+        message += "\nL'acrònim és: "+acronim;
+
+        return message;
+    }
+
+    const frase = "Benvinguts a la vila del pingui";
+    const text = manipulacioText (frase);
+    console.log(text);
+    console.log("-----------------------------------------------")
+
 /* 4. Estadístiques d'un array: Crea una funció que rebi un array de números i retorni un objecte amb:
     • La mitjana 
     • La mediana 
-    • El valor més freqüent (moda) 
-    • La desviació estàndard  */
+    • El valor més freqüent (moda) */
 
 const arrayNums = [1, 3, 10, 18, 2, 7, 22, 5, 2, 4, 27, 6];
 
@@ -27,7 +149,6 @@ let num = 0;
 let mitjana = 0;
 let mediana = 0;
 let moda = 0;
-let desviacioEst = 0;
 
 let posicioMig = 0;
 
@@ -55,15 +176,68 @@ function calculsNums(arrayNums) {
 
         mediana = arrayOrdenat[Math.floor(posicioMig)];
     }
-
     //---------------------------------------------------
 
-    return arrayOrdenat + " / " + largo + " / " + posicioMig + " / " + mediana;
+    const listaCount={};
+
+    arrayOrdenat.map(function(elemento){
+        if (listaCount[elemento]){
+            listaCount[elemento]+=1;
+        } else{
+            listaCount[elemento]=1;
+        }
+    }
+    );
+
+    const convertirArray = Object.entries(listaCount).sort(
+        function (elementA, elementB){
+            return elementA[1] - elementB[1];
+        }
+    );
+
+    const moda= convertirArray[convertirArray.length - 1];
+
+    return "La mitjana és "+mitjana+", La mediana és "+mediana+" i la moda és "+moda;
 }
 
 const solucio = calculsNums(arrayNums);
 console.log(solucio);
+console.log("-----------------------------------------------")
+
 /* 5. Generador de contrasenyes: Crea una funció que generi contrasenyes aleatòries amb:
     • Longitud personalitzable 
     • Opció d'incloure majúscules, minúscules, números i símbols 
     • Garanteix que compleix els requisits seleccionats  */
+
+    function generadorContrasenya (longitud, majuscula, minuscula, numeros, simbol){
+        let contrasenya = "";
+        const majuscules = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const minuscules = "abcdefghijklmnopqrstuvwxyz";
+        const nums = "0123456789";
+        const simbols = "@#$%&";
+
+        let contrasenyaArray = [];
+        if (majuscula) contrasenyaArray.push(majuscules);
+        if (minuscula) contrasenyaArray.push(minuscules);
+        if (numeros) contrasenyaArray.push(nums);
+        if (simbol) contrasenyaArray.push(simbols);
+
+        contrasenyaArray = contrasenyaArray.join("");
+
+        for (let i=0; i<longitud; i++){
+            const aleatori = contrasenyaArray[Math.floor(Math.random() * contrasenyaArray.length)];
+            contrasenya += aleatori;
+        }
+
+        return contrasenya;
+    }
+
+const longitud = 10;
+const majuscula= true;
+const minuscula= true;
+const numeros = false;
+const simbol = true;
+
+const ferContrasenya = generadorContrasenya (longitud, majuscula, minuscula, numeros, simbol);
+
+console.log (ferContrasenya);
--- M6-entornClient/ActExamenRA2-3/intermedis.js
+++ M6-entornClient/ActExamenRA2-3/intermedis.js
@@ -2,6 +2,40 @@
     • Comprova el format (8 números + lletra o NIE) 
     • Valida que la lletra sigui correcta segons l'algoritme oficial 
     • Retorna si és vàlid i el tipus de document  */
+    function validarDNI(dni) {
+        const numDni = dni.slice(0, -1);
+        const lletraDni = dni.slice(-1).toUpperCase();
+        const lletras = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+        let validesa=false;
+        let tipusDocument = "";
+
+        if (numDni.length === 8) {
+            
+            const formatDNI =/[0-9]{8}/;
+
+            if (numDni.match(formatDNI)) {
+                const indexLletra = parseInt(numDni) % 23;
+                const lletraCorrecta = lletras.charAt(indexLletra);
+                
+                if (lletraDni === lletraCorrecta) {
+                    tipusDocument = "DNI";
+                    validesa=true;
+                }
+
+            } else {
+                tipusDocument ="No és un DNI vàlid";
+            }
+        }
+
+        return "El document és de tipus: "+tipusDocument+". Vàlid: "+validesa;
+    }
+
+    const dni = "38871607C";  //Exemple de DNI a validar
+    const validacio= validarDNI(dni);
+    console.log("-----------------------------------------------");
+    console.log(validacio);
+    console.log("-----------------------------------------------");
 
 /* 7. Sistema de notes: Crea un sistema que gestioni notes d'estudiants:
     • Array d'objectes amb nom, cognoms i array de notes 
@@ -9,11 +43,108 @@
     • Ordena els estudiants per nota mitjana 
     • Mostra qui ha suspès (< 5), aprovat (5-6.9), notable (7-8.9) o excel·lent (9-10)  */
 
+    function sistemaNotes(estudiants){        
+        let mitjanaEstudiants = [];
+
+        let mitjana=0;
+        let sumaNotes;
+
+        for (let i=0; i<estudiants.length; i++){
+            sumaNotes = 0;
+            sumaNotes += estudiants[i].notes.reduce((a, b) => a + b, 0);
+
+            mitjana = Math.round(sumaNotes / estudiants[i].notes.length);
+
+            mitjanaEstudiants.push({
+                nom: estudiants[i].nom,
+                cognoms: estudiants[i].Cognoms,
+                mitjana: mitjana
+            });
+        }
+
+        mitjanaEstudiants.sort((a, b) => b.mitjana - a.mitjana);
+        
+        for (let j=0; j<mitjanaEstudiants.length; j++){
+            let qualificacio="";
+
+            if(mitjanaEstudiants[j].mitjana < 5){
+                qualificacio="suspès";
+            } else if (mitjanaEstudiants[j].mitjana < 7) {
+                qualificacio= "aprovat";
+            } else if (mitjanaEstudiants[j].mitjana < 9){
+                qualificacio="notable";
+            } else {
+                qualificacio="excel·lent"
+            }
+
+            mitjanaEstudiants[j].qualificacio = qualificacio;
+        }
+
+        return mitjanaEstudiants;
+    }
+
+    const estudiants = [
+        {nom:"Ariadna", Cognoms:"Pérez Martínez", notes:[5, 7.2, 4.8, 8, 6.5]},
+        {nom:"Marcel", Cognoms:"Fernandez Llopis", notes:[6.7, 10, 7.3, 8, 7]},
+        {nom:"Llucia", Cognoms: "Ramonet González", notes:[7, 8, 4.1, 9.2, 6]},
+        {nom:"Oriol", Cognoms:"Massuet Aguirre", notes:[7, 6, 8.4, 6.6, 8]},
+        {nom:"Sofia", Cognoms:"Lopez Torres", notes:[9, 7.5, 4.3, 8, 9.7]}
+    ]
+
+    const returnSistema = sistemaNotes(estudiants);
+    console.log(returnSistema);
+    console.log("-----------------------------------------------");
+
 /* 8. Conversor de temps: Crea funcions per convertir:
     • Segons a format HH:MM:SS 
     • Dies, hores, minuts a segons totals 
     • Format 12h a 24h i viceversa 
     • Calcula la diferència entre dues hores en format llegible */
+
+    function conversorTemps(segons){
+        const minuts = Math.floor(segons / 60);
+        const hores = Math.floor(minuts / 60);
+
+        const segonsRestants = segons % 60;
+        const minutsRestants = minuts % 60;
+        const horesRestants = hores % 24;  
+
+        const formatHHMMSS = horesRestants.toString().padStart(2, '0') + ':' + minutsRestants.toString().padStart(2, '0') + ':' +
+                             segonsRestants.toString().padStart(2, '0');
+
+        // Dies, hores, minuts a segons totals
+        const dies = 3;
+        const hores2= 4;
+        const minuts2 = 47;
+
+        const totalSegons = (dies * 86400) + (hores2 * 3600) + (minuts2 * 60);
+
+        // calcular la diferència entre dues hores en format llegible
+        const data1 = new Date();
+        let horaActual= data1.getHours();
+        let minutActual= data1.getMinutes();
+        let segonActual= data1.getSeconds();
+
+        const data2 = new Date();
+        data2.setHours(12);
+        data2.setMinutes(30);
+        data2.setSeconds(15);
+
+        let horaDiferencia = Math.abs(data2.getHours() - horaActual);
+        let minutDiferencia = Math.abs(data2.getMinutes() - minutActual);
+        let segonDiferencia = Math.abs(data2.getSeconds() - segonActual);  
+
+        const difHora = horaDiferencia.toString().padStart(2, '0') + ':' + minutDiferencia.toString().padStart(2, '0') + ':' +
+                        segonDiferencia.toString().padStart(2, '0');
+        
+    
+        return formatHHMMSS+"\n"+totalSegons+"\n"+"la diferencia entre "+ horaActual+":"+minutActual+":"+segonActual+" i 12:30:15 és "+difHora;
+    }
+
+    const segons = 3665;
+    const resultFunction = conversorTemps(segons);
+    console.log(resultFunction);
+    console.log("-----------------------------------------------");
 
 /* 9. Filtrador d'objectes: Donat un array d'objectes de productes amb propietats (nom, preu, categoria, stock):
     • Filtra per rang de preus 
